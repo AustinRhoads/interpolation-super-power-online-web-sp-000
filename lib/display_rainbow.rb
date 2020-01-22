669c7a818ad2aa92b7126991a24cbd5b708728d3
Austin Rhoads <austinrrhoads@ymail.com>
2020-01-22 22:07:25 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+def display_rainbow(ary)
+  puts "R: #{ary[0]}"
+  puts "O: #{ary[1]}"
+  puts "Y: #{ary[2]}"
+  puts "G: #{ary[3]}"
+  puts "B: #{ary[4]}"
+  puts "I: #{ary[5]}"
+  puts "V: #{ary[6]}"
+end
+colors = ["red","orange","yellow","green","blue","indigo","violet"]
